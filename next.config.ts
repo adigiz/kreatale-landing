fd1fb6f0b130ebdca9f23d83534fc13eebc0ff84
Giz <adigiz@yahoo.com>
2025-11-30 09:39:23 +0700

    fix: missing next config

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "s3.kreatale.com",
-          pathname: "/**",
-        },
-      ],
+  images: {
     remotePatterns: [
       {
         protocol: "https",
